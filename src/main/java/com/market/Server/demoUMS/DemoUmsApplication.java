22868ee2dbfe3236f5fcb0594514d399bcdd7291
junshock5 <61732452+junshock5@users.noreply.github.com>
2020-06-06 15:03:08 +0900

    create market.server.demoUMS

---
+++ src/main/java/com/market/Server/demoUMS/DemoUmsApplication.java
@@ -0,0 +1,13 @@
+package com.market.Server.demoUMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoUmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoUmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/market/Server/demoUMS/DemoUmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.Server.demoUMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoUmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
